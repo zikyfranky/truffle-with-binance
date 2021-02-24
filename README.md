04f6b48f3c4b9a608ec278dbc4ad155eed0e21fb
Isaac Frank <masturisaacfrank@gmail.com>
2021-02-24 20:48:37 +0100

    added verification ability for both networks

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ The `.env.example` file contains all information to make this script run perfect
 ```js
 BSC_PRIVATE_KEY; // Variable responsible for the private key of the `SmartChain` address you wish to deploy with.
 BSC_SEED_PHRASE; // Your `SmartChain` wallet 12 mnemonic phrases, first account will be used to deploy the contract by default
+BSC_API_KEY; // Your `SmartChain` API KEY gotten from bscscan.com/myapikey, this is used to verify deployed contracts on testnet and mainnet
 ```
 
 ##### ETHEREUM CONFIGURATIONS
@@ -16,6 +17,7 @@ BSC_SEED_PHRASE; // Your `SmartChain` wallet 12 mnemonic phrases, first account 
 ```js
 ETH_PRIVATE_KEY; // Variable responsible for the private key of the `Ethereum` address you wish to deploy with.
 ETH_SEED_PHRASE; // Your `Ethereum` wallet 12 mnemonic phrases, first account will be used to deploy the contract by default
+ETH_API_KEY; // Your `Ethereum` API KEY gotten from etherscan.com/myapikey, this is used to verify deployed contracts on testnet and mainnet
 INFURA_PROJECT_ID; //This should hold your infura project id
 ```
 
@@ -46,5 +48,17 @@ To deploy a contract you first need to create a migration file that deploys your
 `truffle test --network <network_name>`
 
 Where `network_name` is any of the above mentioned network.
+
+###### Verify deployed contracts on testnet or mainnet
+
+`truffle run verify <contract>@<contract_address> --network <network_name>`
+
+Where:
+
+1. `contract` is the name of the deployed contract you wish to verify.
+2. `contract_address` is the address of the deployed contract you wish to verify.
+3. `network_name` is any of the above mentioned network.
+
+E.G: I want to verify a contract named ZeekToken and the address is 0x3582AFa8dfaF9e2072467908392459CF184a9399 deployed on the bsc mainet, I'll simply run `truffle run verify ZeekToken@0x3582AFa8dfaF9e2072467908392459CF184a9399 --network bsc_main`;
 
 BTW I have a playlist of this repo which you can find here: [Contracts Development on Binance Smart Chain with Truffle](https://www.youtube.com/playlist?list=PLG3Tf5BKdISnT1NjbIuAgRzvwnTs72p-F)
--- truffle-config.js
+++ truffle-config.js
@@ -1,52 +1,72 @@
-require('dotenv').config();
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    bscscan: process.env.BSC_API_KEY,
+    etherscan: process.env.ETH_API_KEY,
+  },
   networks: {
-
     //GANACHE NETWORK
     ganache: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
     },
 
     // ETHEREUM TESTNET
     eth_test: {
-      provider: () => new HDWalletProvider(process.env.ETH_SEED_PHRASE || process.env.ETH_PRIVATE_KEY, `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`),
-      network_id: 3,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 500,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      provider: () =>
+        new HDWalletProvider(
+          process.env.ETH_SEED_PHRASE || process.env.ETH_PRIVATE_KEY,
+          `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 500, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
 
     // ETHEREUM MAINNET
     eth_main: {
-      provider: () => new HDWalletProvider(process.env.ETH_SEED_PHRASE || process.env.ETH_PRIVATE_KEY, `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`),
-      network_id: 1,       // Mainnet's id
-      gas: 1000000,        // Mainnet has a higher block limit than ropsten
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 500,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      provider: () =>
+        new HDWalletProvider(
+          process.env.ETH_SEED_PHRASE || process.env.ETH_PRIVATE_KEY,
+          `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        ),
+      network_id: 1, // Mainnet's id
+      gas: 1000000, // Mainnet has a higher block limit than ropsten
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 500, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
 
     //BINANCE SMART CHAIN TESTNET
     bsc_test: {
-      provider: () => new HDWalletProvider(process.env.BSC_SEED_PHRASE || process.env.BSC_PRIVATE_KEY, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.BSC_SEED_PHRASE || process.env.BSC_PRIVATE_KEY,
+          `https://data-seed-prebsc-1-s1.binance.org:8545`
+        ),
       network_id: 97,
       confirmations: 2,
       timeoutBlocks: 500,
-      skipDryRun: true
+      skipDryRun: true,
     },
 
     //BINANCE SMART CHAIN MAINNET
     bsc_main: {
-      provider: () => new HDWalletProvider(process.env.BSC_SEED_PHRASE || process.env.BSC_PRIVATE_KEY, `https://bsc-dataseed1.binance.org`),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.BSC_SEED_PHRASE || process.env.BSC_PRIVATE_KEY,
+          `https://bsc-dataseed1.binance.org`
+        ),
       network_id: 56,
       confirmations: 10,
       timeoutBlocks: 200,
-      skipDryRun: true
+      skipDryRun: true,
     },
   },
 
@@ -55,17 +75,17 @@ module.exports = {
     // timeout: 100000
   },
 
-
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.6.8",    // Fetch exact version from solc-bin (default: truffle's version)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
-       optimizer: {
-         enabled: false,
-         runs: 200
-       },
-      }
-    }
-  }
+      version: "0.6.8", // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200,
+        },
+      },
+    },
+  },
 };
